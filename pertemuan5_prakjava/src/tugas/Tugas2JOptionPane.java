8c1241d66f27ec9d3ba515d35b4cb7d907401e2e
Heri Ramadhan <43706967+21343050@users.noreply.github.com>
2022-09-16 15:15:13 +0700

    Add files via upload

---
+++ pertemuan5_prakjava/src/tugas/Tugas2JOptionPane.java
@@ -0,0 +1,15 @@
+package tugas;
+
+import javax.swing.JOptionPane;
+
+public class Tugas2JOptionPane {
+    public static void main(String[] args) {
+        String word1 = "", word2 = "", word3 = "";
+
+        word1 = JOptionPane.showInputDialog("Enter word 1");
+        word2 = JOptionPane.showInputDialog("Enter word 2");
+        word3 = JOptionPane.showInputDialog("Enter word 3");
+
+        String msg = 
+    }
+}
