c8365c949e27d797dfe5ac8f4f20bd54e6fb0540
Heri Ramadhan <43706967+21343050@users.noreply.github.com>
2022-09-23 15:16:31 +0700

    Add files via upload

---
+++ JS6/src/latihan/Grade2.java
@@ -0,0 +1,22 @@
+package latihan;
+
+public class Grade2 {
+    public static void main(String[] args) {
+        int grade = 92;
+
+        switch (grade) {
+            case 100:
+                System.out.println("Exellent!");
+                break;
+            case 90:
+                System.out.println("Good job!");
+                break;
+            case 80:
+                System.out.println("Study harder!");
+                break;
+            default:
+                System.out.println("Sorry, you failed.");
+                break;
+        }
+    }
+}
